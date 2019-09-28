f528a627af3e3c1f7d9f620b726da7e460589caa
Autofire <Autofire@users.noreply.github.com>
2019-09-28 09:13:24 -0700

    Pulled in tile stuff from Omnipresent; did player.

---
+++ Assets/Scripts/Player/BodyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReachBeyond.Player {
+
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class BodyController : MonoBehaviour {
+
+#pragma warning disable CS0649
+		[SerializeField] private float walkSpeed;
+#pragma warning restore CS0649
+
+		private Rigidbody2D rb;
+
+		private void Awake() {
+			rb = GetComponent<Rigidbody2D>();
+		}
+		public void Walk(float xMagnitude) {
+			rb.velocity =
+				Vector2.up * rb.velocity +
+				Vector2.right * walkSpeed * xMagnitude;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerBrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace ReachBeyond.Player {
+	public class PlayerBrain : MonoBehaviour {
+		public BodyController body;
+		public string inputAxis = "Horizontal";
+
+		private void Awake() {
+			Assert.IsNotNull(body);
+		}
+
+		private void Update() {
+			body.Walk(Input.GetAxis(inputAxis));
+		}
+	}
+}
