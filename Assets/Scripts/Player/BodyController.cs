a1c29a4e2dc718365489ce40a9e94da0706a9937
Autofire <Autofire@users.noreply.github.com>
2019-09-29 06:35:34 -0700

    Imported animations and stuff.

--- Assets/Scripts/Player/BodyController.cs
+++ Assets/Scripts/Player/BodyController.cs
@@ -7,19 +7,38 @@ namespace ReachBeyond.Player {
 	[RequireComponent(typeof(Rigidbody2D))]
 	public class BodyController : MonoBehaviour {
 
-#pragma warning disable CS0649
-		[SerializeField] private float walkSpeed;
-#pragma warning restore CS0649
+		public float baseWalkSpeed = 1.9f;
+		public float walkSpeedMod = 1.0f;
+
+		[Space(10)]
+		public Animator animController;
+		public SpriteRenderer spriteRenderer;
+		public float walkDeadzone = 0.05f;
+		public string standAnim = "Idle";
+		public string walkAnim = "Walk";
+		public string walkSpeedVar = "WalkSpeed";
 
 		private Rigidbody2D rb;
 
 		private void Awake() {
 			rb = GetComponent<Rigidbody2D>();
 		}
+
 		public void Walk(float xMagnitude) {
 			rb.velocity =
 				Vector2.up * rb.velocity +
-				Vector2.right * walkSpeed * xMagnitude;
+				Vector2.right * baseWalkSpeed * walkSpeedMod * xMagnitude;
+
+			float absMag = Mathf.Abs(xMagnitude);
+
+			if(absMag > walkDeadzone) {
+				spriteRenderer.flipX = xMagnitude > 0;
+				animController.SetFloat(walkSpeedVar, absMag * walkSpeedMod);
+				animController.Play(walkAnim);
+			}
+			else {
+				animController.Play(standAnim);
+			}
 		}
 	}
 }
